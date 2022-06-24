89ff7b0aa6f6f328f3e08cacb90985b4d60d10bb
Danish Javed <danishjaved636@gmail.com>
2022-06-24 23:44:50 +0530

    Added dark mode

--- Js/index.js
+++ Js/index.js
@@ -1,12 +1,11 @@
 
 const toogleMode= ()=>{
     let body = document.body;
-    body.classList.toggle('background-color-change');
-    body.classList.toggle('bg-dark');
-    body.classList.toggle('text-light');
-    body.classList.toggle('bg-gradient');
+    body.classList.toggle('dark');
+    document.getElementById('lightThemeIcon').classList.toggle('d-none');
+    document.getElementById('darkThemeIcon').classList.toggle('d-none');
 
-    if (body.classList.contains('bg-dark')) {
+    if (body.classList.contains('dark')) {
         let darkButtons = document.getElementsByClassName('btn-outline-dark');
         [...darkButtons].forEach(element => {
             element.classList.remove('btn-outline-dark');
