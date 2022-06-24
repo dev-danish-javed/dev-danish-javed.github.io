4938b4bcb05a2b127ec9a05d3e764cdb342523f0
Danish Javed <danishjaved636@gmail.com>
2022-06-24 22:14:01 +0530

    Phone Compatible

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,25 @@
-const showRough = () => {
-    [...document.getElementsByClassName('frame')].forEach(element => {
-        element.classList.toggle('d-none');
-    });
+
+const toogleMode= ()=>{
+    let body = document.body;
+    body.classList.toggle('background-color-change');
+    body.classList.toggle('bg-dark');
+    body.classList.toggle('text-light');
+    body.classList.toggle('bg-gradient');
+
+    if (body.classList.contains('bg-dark')) {
+        let darkButtons = document.getElementsByClassName('btn-outline-dark');
+        [...darkButtons].forEach(element => {
+            element.classList.remove('btn-outline-dark');
+            element.classList.add('btn-outline-light');
+        });
+        
+    }
+    else {
+        let lightButtons = document.getElementsByClassName('btn-outline-light');
+        [...lightButtons].forEach(element => {
+            element.classList.remove('btn-outline-light');
+            element.classList.add('btn-outline-dark');
+        });
+    }
+
 }
